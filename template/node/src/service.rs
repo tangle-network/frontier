a238c9f1ca64fb91cc5c1ab16908f335edb1d849
Drew Stone <drewstone329@gmail.com>
2021-05-29 17:56:05 -0400

    Ups substrate commit

--- template/node/src/service.rs
+++ template/node/src/service.rs
@@ -40,15 +40,10 @@ type FullSelectChain = sc_consensus::LongestChain<FullBackend, Block>;
 
 #[cfg(feature = "aura")]
 pub type ConsensusResult = (
-	sc_consensus_aura::AuraBlockImport<
+	FrontierBlockImport<
 		Block,
-		FullClient,
-		FrontierBlockImport<
-			Block,
-			sc_finality_grandpa::GrandpaBlockImport<FullBackend, Block, FullClient, FullSelectChain>,
-			FullClient
-		>,
-		AuraPair
+		sc_finality_grandpa::GrandpaBlockImport<FullBackend, Block, FullClient, FullSelectChain>,
+		FullClient
 	>,
 	sc_finality_grandpa::LinkHalf<Block, FullClient, FullSelectChain>
 );
@@ -166,16 +161,12 @@ pub fn new_partial(config: &Configuration, #[allow(unused_variables)] cli: &Cli)
 			frontier_backend.clone(),
 		);
 
-		let aura_block_import = sc_consensus_aura::AuraBlockImport::<_, _, _, AuraPair>::new(
-			frontier_block_import, client.clone(),
-		);
-
 		let slot_duration = sc_consensus_aura::slot_duration(&*client)?;
 		let raw_slot_duration = slot_duration.slot_duration();
 
 		let import_queue = sc_consensus_aura::import_queue::<AuraPair, _, _, _, _, _, _>(
 			ImportQueueParams {
-				block_import: aura_block_import.clone(),
+				block_import: frontier_block_import.clone(),
 				justification_import: Some(Box::new(grandpa_block_import.clone())),
 				client: client.clone(),
 				create_inherent_data_providers: move |_, ()| async move {
@@ -203,7 +194,7 @@ pub fn new_partial(config: &Configuration, #[allow(unused_variables)] cli: &Cli)
 			client, backend, task_manager, import_queue, keystore_container,
 			select_chain, transaction_pool,
 			other: (
-				(aura_block_import, grandpa_link),
+				(frontier_block_import, grandpa_link),
 				pending_transactions,
 				filter_pool,
 				frontier_backend,
@@ -227,7 +218,7 @@ pub fn new_full(
 		other: (consensus_result, pending_transactions, filter_pool, frontier_backend, mut telemetry),
 	} = new_partial(&config, cli)?;
 
-	let (network, network_status_sinks, system_rpc_tx, network_starter) =
+	let (network, system_rpc_tx, network_starter) =
 		sc_service::build_network(sc_service::BuildNetworkParams {
 			config: &config,
 			client: client.clone(),
@@ -308,7 +299,6 @@ pub fn new_full(
 			on_demand: None,
 			remote_blockchain: None,
 			backend,
-			network_status_sinks,
 			system_rpc_tx,
 			config,
 			telemetry: telemetry.as_mut(),
@@ -462,13 +452,12 @@ pub fn new_full(
 			};
 
 			let grandpa_config = sc_finality_grandpa::Config {
-				// FIXME #1578 make this available through chainspec
-				gossip_duration: Duration::from_millis(333),
+				gossip_duration: std::time::Duration::from_millis(333),
 				justification_period: 512,
 				name: Some(name),
 				observer_enabled: false,
 				keystore,
-				is_authority: role.is_authority(),
+				local_role: role.clone(),
 				telemetry: telemetry.as_ref().map(|x| x.handle()),
 			};
 
@@ -543,16 +532,12 @@ pub fn new_light(config: Configuration) -> Result<TaskManager, ServiceError> {
 		telemetry.as_ref().map(|x| x.handle()),
 	)?;
 
-	let aura_block_import = sc_consensus_aura::AuraBlockImport::<_, _, _, AuraPair>::new(
-		grandpa_block_import.clone(),
-		client.clone(),
-	);
 
 	let slot_duration = sc_consensus_aura::slot_duration(&*client)?.slot_duration();
 
 	let import_queue = sc_consensus_aura::import_queue::<AuraPair, _, _, _, _, _, _>(
 		ImportQueueParams {
-			block_import: aura_block_import.clone(),
+			block_import: grandpa_block_import.clone(),
 			justification_import: Some(Box::new(grandpa_block_import.clone())),
 			client: client.clone(),
 			create_inherent_data_providers: move |_, ()| async move {
@@ -583,7 +568,7 @@ pub fn new_light(config: Configuration) -> Result<TaskManager, ServiceError> {
 
 	let rpc_extensions = crate::rpc::create_light(light_deps);
 
-	let (network, network_status_sinks, system_rpc_tx, network_starter) =
+	let (network, system_rpc_tx, network_starter) =
 		sc_service::build_network(sc_service::BuildNetworkParams {
 			config: &config,
 			client: client.clone(),
@@ -611,7 +596,6 @@ pub fn new_light(config: Configuration) -> Result<TaskManager, ServiceError> {
 		keystore: keystore_container.sync_keystore(),
 		backend,
 		network,
-		network_status_sinks,
 		system_rpc_tx,
 		telemetry: telemetry.as_mut(),
 	})?;
